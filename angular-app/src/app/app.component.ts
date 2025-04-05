1698022f5df1ba7277ede849997a796692a50f57
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-04-06 01:08:39 +0530

    making my first ts change and defining the difference between variable and property(interview question)

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -9,5 +9,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'angular-app';// this is property we can not define the variable directly inside class
+  hello() {
+    let c = 10;//making my variable
+  }
 }
