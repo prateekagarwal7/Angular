140d144cab3849ab9f43563bc99158dd630f0939
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-04-07 01:41:48 +0530

    datatype practical

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -13,8 +13,13 @@ import { Profile } from './profile/profile.component';
 export class AppComponent {
   title = 'angular-app';// this is property we can not define the variable directly inside class
   age = 10;
+  surname: string = "prateek";// this is ised to assing the data type to properties
+  address: string | number = "rrr";// either of the two specifies data type can be put inside it
+  others: any;//any data type can be put inside it
   hello() {
     let c = 10;//making my variable
+    this.others = true;
+    this.address = 123;
   }
   clickmebtn() {
     //console.log("function for the button cick");
