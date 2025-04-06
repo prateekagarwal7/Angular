a6c0e6d0f3b727d1751cc90600d8b209644970f8
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-04-07 00:50:35 +0530

    using the component

--- angular-app/src/app/login/login.component.ts
+++ angular-app/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login',// this will work as the html tag  we can use this selector invoke the component in parent component
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
