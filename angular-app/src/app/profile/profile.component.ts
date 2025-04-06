aae7b67e0d5f21a4dda1b4a5c601c2df8cbfbf52
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-04-07 01:00:01 +0530

    making of the custom component

---
+++ angular-app/src/app/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';// we are injecting the dependencies 
+
+
+@Component({
+  selector: 'app-profile',
+  template:'<h1>Profile component</h1>'// these 2 are necessary to make a component 
+})
+export class Profile {
+  // it is the class of the component
+
+}
