be4112ca541b6c547e03543b4650d56d90f0bebd
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-04-23 01:29:42 +0530

    ngIf directive

--- angular-app/src/app/todo/todo.component.ts
+++ angular-app/src/app/todo/todo.component.ts
@@ -1,10 +1,11 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
@@ -12,6 +13,7 @@ export class TodoComponent {
   task: string = "";
   color = 'red';
   value = true;
+  login = true;
   taskList: { id: number, task: string }[] = []//making the list of object type
   add() {
     this.taskList.push({ id: this.taskList.length + 1, task: this.task })//example of pushing a object in the list
