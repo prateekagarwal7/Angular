5b07a961332b469d380043669215b1ce8431c94f
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-05-06 02:19:16 +0530

    validation changes for the template file

--- angular-app/src/app/template-form/template-form.component.ts
+++ angular-app/src/app/template-form/template-form.component.ts
@@ -1,10 +1,11 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
   standalone: true,
-  imports: [ FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.css'
 })
