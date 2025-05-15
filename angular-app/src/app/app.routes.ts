be9b5e949afc5d2778a0eb7559d1c3b9a5d39b93
Prateek Agarwal <prateekagarwal246@gmail.com>
2025-05-16 00:10:27 +0530

    changes for the lazy loading

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -3,9 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ContactComponent } from './contact/contact.component';
 import { UserComponent } from './user/user.component';
+import { LazyComponent } from './lazy/lazy.component';
+
 
 
 export const routes: Routes = [{ path: 'Home', data: { name: 'cwhw' } ,component: HomeComponent},
   { path: 'contact', component: ContactComponent },
   { path: 'user/:id/:name', component: UserComponent },
+  {
+    path: 'lazy', loadComponent: () => import('./lazy/lazy.component' ).then((c) => c.LazyComponent)// it will be used to add the lazy loading in the application
+  },
 { path: '**', component: PageNotFoundComponent }];
